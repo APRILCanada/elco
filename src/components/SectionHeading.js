b8c2d6bdc7928ca4e5e79767a62c402fe718cd10
april.outofbox@gmail.com <april.outofbox@gmail.com>
2022-07-25 15:50:49 -0400

    shape icon

--- src/components/SectionHeading.js
+++ src/components/SectionHeading.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import styled from 'styled-components'
 import PropTypes from 'prop-types';
+import ShapeIcon from '../assets/icons/shape';
 
 const SectionHeading = ({ children }) => {
     return (
         <Heading>
-            <svg width="62" height="62" viewBox="0 0 62 62" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M1 30C1 13.9837 13.9837 1 30 1H32C48.0163 1 61 13.9837 61 30V61H30C13.9837 61 1 48.0163 1 32V30Z" fill="#013D62" stroke="#013D62" strokeWidth="2" />
-            </svg>
+            <ShapeIcon />
             {children}
         </Heading>
     )
